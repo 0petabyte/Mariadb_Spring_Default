08a311043dd4d85d92024ad15e5ea5fea0a9672c
0petabyte <0petabyte@gmail.com>
2020-03-07 14:44:00 +0900

    Mariadb 기본 디비 연동 셋팅 완료

---
+++ MariaDB_Spring_Default/src/test/java/com/peta/home/DbGetTest.java
@@ -0,0 +1,44 @@
+package com.peta.home;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import javax.inject.Inject;
+import javax.sql.DataSource;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations= {"file:src/main/webapp/WEB-INF/spring/**/root-context.xml"})
+public class DbGetTest {
+	@Inject
+	private DataSource ds;
+
+	private PreparedStatement pstmt;
+	
+	@Test
+	public void testConection()throws Exception{
+		try(Connection con = ds.getConnection()){
+			System.out.println("디비연결성공");
+			String query = "show tables";
+			pstmt = con.prepareStatement(query);
+			ResultSet rs = pstmt.executeQuery(query);
+			while(rs.next()) {
+				String id = rs.getString("Tables_in_fav_data");
+				System.out.println(id);
+			}
+			
+		}catch(Exception e) {
+			System.out.println("디비연결실패");
+			e.printStackTrace();
+		}
+		
+	}
+	
+}
